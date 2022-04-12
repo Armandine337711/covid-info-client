af34b1730dae223cdcbdc6c3263a95e3842ddb7c
Armandine BARGE <armandine.barge@ntymail.com>
2022-04-12 10:13:00 +0200

    Navbar

--- c19-client/pages/index.jsx
+++ c19-client/pages/index.jsx
@@ -2,14 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Header />
       <h1>Covid Info - Les actualités</h1>
-      <Link href="/contact">
-        <a>Contact</a>
-      </Link>
     </div>
   );
 }
